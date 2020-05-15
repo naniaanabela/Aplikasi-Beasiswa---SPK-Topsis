b6d3565c0066c10367853250189ddb2d49c7b5e3
Nurcahya Nania Anabela <nania.anabela22@gmail.com>
2020-05-15 20:16:10 +0700

    index

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+//koneksi
+session_start();
+include "koneksi.php";
+
+ ?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>SPK TOPSIS</title>
+    <!--bootstrap-->
+    <link href="tampilan/css/bootstrap.min.css" rel="stylesheet">
+    <link href="styles/slider.css" rel="stylesheet" type="text/css" media="all">
+
+  </head>
+  <body><br>
+<!--navbar navbar-default navbar-custom-->
+    <!--menu-->
+    <nav class="">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Sistem Pendukung Keputusan Metode Topsis</a>
+        </div>
+
+        <div class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li>
+              <a href="kriteria.php">Kriteria</a>
+            </li>
+            <li>
+              <a href="alternatif.php">Alternatif</a>
+            </li>
+            <li>
+              <a href="nilmat.php">Nilai Matriks</a>
+            </li>
+            <li>
+              <a href="hastop.php">Hasil Topsis</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav><br><br>
+
+    <div class="container">
+      <center><h1><bold>SELAMAT DATANG DI WEBSITE SPK PENERIMAAN BEASISWA PENINGKATAN PRESTASI AKADEMIK (PPA)</bold></h1></center>
+    </div>
+
+    <!-- <div id="slider" >
+      <figure>
+        <img src="gambar/banner1.png">
+        <img src="gambar/banner2.png">
+        <img src="gambar/banner3.png">
+        <img src="gambar/banner2.png">
+      </figure>
+    </div> <br><br> -->
+    <!--footer-->
+    <!-- <footer class="text-center">
+      <div class="footer-below">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <em>Sistem Pendukung Keputusan Pemilihan Penerima Beasiswa Dengans Metode Topsis</em>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer> -->
+
+    <!--plugin-->
+    <script src="tampilan/js/bootstrap.min.js"></script>
+
+  </body>
+</html>
